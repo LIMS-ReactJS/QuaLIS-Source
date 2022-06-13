84174760d98d528e7eb34584b2d1a2e743e581ec
ATE186 <murali.r@agaramtech.com>
2022-06-13 12:13:17 +0530

    #24
    Create POM for login page

---
+++ ObjectRepository/Login/ElementLogin.py
@@ -0,0 +1,11 @@
+loginid="//input[@id='sloginid']"
+
+password="//input[@id='spassword']"
+
+login="//button[text()='Login']"
+
+welcomeText="//h1[text()='Welcome to Qualis']"
+
+userRole="//div[@class='react-select__control css-yk16xz-control']"
+
+pin ="//a[@class='text-center nav-link']"
