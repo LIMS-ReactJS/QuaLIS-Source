15819001d7a973a910907f81c44289b74e4f712c
ATE186 <murali.r@agaramtech.com>
2022-06-13 12:56:47 +0530

    #25
    Add maximize feature in the launch browser method

--- Utilities/BrowserOperation.py
+++ Utilities/BrowserOperation.py
@@ -4,11 +4,12 @@
 
 import time
 from selenium import webdriver
-
+from Utilities import Utility
 
 def launchBrowser():
     global driver
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--incognito")
     driver = webdriver.Chrome(executable_path= "..\\chromedriver.exe",options=chrome_options)
+    Utility.maximize(driver)
     return driver
