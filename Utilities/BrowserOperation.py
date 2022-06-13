a1b78242ab368ff4c390671456a35190e34e5bdd
ATE186 <murali.r@agaramtech.com>
2022-06-13 11:03:41 +0530

    #20
    Need to remove unwanted lines in the launch method in Browser Operation

--- Utilities/BrowserOperation.py
+++ Utilities/BrowserOperation.py
@@ -8,14 +8,5 @@ from Utilities import Utility
 
 def launchBrowser():
     global driver
-
     driver = webdriver.Chrome("..\\chromedriver.exe")
 
-    driver.get("https://www.google.com/")
-
-    time.sleep(5)
-
-    driver.refresh()
-
-
-launchBrowser()
