d8dcf4146e3d2dd770f4e1a7b4750872d19fbe5e
ATE186 <87387589+ATE186@users.noreply.github.com>
2022-06-29 17:10:36 +0530

    #38, #79

---
+++ ObjectRepository/Contact/ElementClient.py
@@ -0,0 +1,50 @@
+########################################################################################################################################################################################################################################################################################################################################################
+# Author: ATE186, Date: 29th June 2022, ID - #79 ||
+########################################################################################################################################################################################################################################################################################################################################################
+
+
+from Setting import ObjectName
+
+pin = "//a[@class='text-center nav-link']"
+
+masterIcon="//a[@data-rb-event-key='MenuId_1']"
+
+contactsIcon="//span[text()='Contacts']"
+
+clientIcon="//a[@href='#/client'][@nformcode='50']"
+
+clientAdd="//button[@data-tip='Add']"
+
+clientRefresh="//button[@data-tip='Refresh']"
+
+clientDownloadPDF="//button[@data-tip='Download PDF']"
+
+clientDownloadExcel="//button[@data-tip='Download Excel']"
+
+clientName="//input[@id='sclientname'][@name='sclientname']"
+
+clientAddressOne="//*[@id='saddress1'][@name='saddress1']"
+
+clientAddressTwo="//*[@id='saddress2'][@name='saddress2']"
+
+clientAddressThree="//*[@id='saddress3'][@name='saddress3']"
+
+clientCountry="//*[@id='ncountrycode']"
+
+clientPhoneNumber="//input[@id='sphoneno'][@name='sphoneno']"
+
+clientMobileNumber="//input[@id='smobileno'][@name='smobileno']"
+
+clientFaxNumber="//input[@id='sfaxno'][@name='sfaxno']"
+
+clientEmail="//input[@id='semail'][@name='semail']"
+
+clientActive="//*[@for='ntransactionstatus'][text()='Is Active']"
+
+clientAddSubmit="//button[text()='Save']"
+
+clientAddCancel="//button[text()='Cancel']"
+
+clientEditSubmit="//button[text()='Save']"
+
+clientEditCancel="//button[text()='Cancel']"
