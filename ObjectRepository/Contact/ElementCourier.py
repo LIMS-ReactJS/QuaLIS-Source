f2df88d155c961a2afbf830e4dc5dc88b8d098d9
ATE186 <87387589+ATE186@users.noreply.github.com>
2022-06-29 15:47:10 +0530

    #77

---
+++ ObjectRepository/Contact/ElementCourier.py
@@ -0,0 +1,78 @@
+########################################################################################################################################################################################################################################################################################################################################################
+# Author: ATE186, Date: 29th June 2022, ID - #77 ||
+########################################################################################################################################################################################################################################################################################################################################################
+
+
+from Setting import ObjectName
+
+pin = "//a[@class='text-center nav-link']"
+
+masterIcon="//a[@data-rb-event-key='MenuId_1']"
+
+contactsIcon="//span[text()='Contacts']"
+
+courierIcon="//a[@href='#/courier'][@nformcode='49']"
+
+courierAdd="//button[@data-tip='Add']"
+
+courierRefresh="//button[@data-tip='Refresh']"
+
+courierDownloadPDF="//button[@data-tip='Download PDF']"
+
+courierDownloadExcel="//button[@data-tip='Download Excel']"
+
+courierName="//input[@id='scouriername'][@name='scouriername']"
+
+courierContactsPerson="//input[@id='scontactperson'][@name='scontactperson']"
+
+courierAddressOne="//input[@id='saddress1'][@name='saddress1']"
+
+courierAddressTwo="//input[@id='saddress2'][@name='saddress2']"
+
+courierAddressThree="//input[@id='saddress3'][@name='saddress3']"
+
+courierCountry="//input[@id='ncountrycode']"
+
+courierPhoneNumber="//input[@id='sphoneno'][@name='sphoneno']"
+
+courierMobileNumber="//input[@id='smobileno'][@name='smobileno']"
+
+courierFaxNumber="//input[@id='sfaxno'][@name='sfaxno']"
+
+courierEmail="//input[@id='semail'][@name='semail']"
+
+courierAddSubmit="//button[text()='Save']"
+
+courierAddCancel="//button[text()='Cancel']"
+
+courierEditSubmit="//button[text()='Save']"
+
+courierEditCancel="//button[text()='Cancel']"
+
+courierNameList="/html/body/div[1]/div/div[2]/div/div[2]/div/div/div/div[2]/div/div[3]/div/div[1]/table/tbody/tr/td[1]"
+
+courierDescriptionList="//tbody[@role='presentation']/tr/td[2]"
+
+courierDefaultStatusList="//tbody[@role='presentation']/tr/td[3]"
+
+courierEditList="//tbody[@role='presentation']/tr/td[4]/a/span[1]"
+
+courierDeleteList="//tbody[@role='presentation']/tr/td[4]/a/span[2]"
+
+courierNameFilter="(//span[@class='k-icon k-i-more-vertical'])[1]"
+
+courierDefaultStatusLabel="//label[@for='ndefaultstatus']"
+
+courierDefaultStatusToggle="//input[@name='ndefaultstatus' and @type='checkbox']"
+
+courierTotalCount="//div[@class='k-pager-info k-label']"
+
+courierScreenHeader="//*[@class='navbar-brand']/h2"
+
+courierAddPopupHeader="//div[@class='header-primary flex-grow-1 modal-title h4' and @id='add-user']"
+
+module="Master"
+
+subModule="Contacts"
+
+screen="courier"
