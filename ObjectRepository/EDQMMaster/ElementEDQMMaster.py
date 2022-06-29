e134385e58c392cab6fbe80bc6f0cb3f6e4308cd
ATE186 <87387589+ATE186@users.noreply.github.com>
2022-06-29 18:41:56 +0530

    #83

--- ObjectRepository/EDQMMaster/ElementEDQMMaster.py
+++ ObjectRepository/EDQMMaster/ElementEDQMMaster.py
@@ -1,3 +1,37 @@
 ########################################################################################################################################################################################################################################################################################################################################################
 # Author: ATE186, Date: 29th June 2022, ID - #83 ||
 ########################################################################################################################################################################################################################################################################################################################################################
+
+pin = "//a[@class='text-center nav-link']"
+
+masterIcon="//a[@data-rb-event-key='MenuId_1']"
+
+EDQMMasterIcon="//span[text()='EDQM Master']"
+
+EDQMMasterFileTypeIcon="//a[@href='#/edqmMasterFileType'][@nformcode='9']"
+
+EDQMMasterFileTypeAdd="//button[@data-tip='Add']"
+
+EDQMMasterFileTypeRefresh="//button[@data-tip='Refresh']"
+
+EDQMMasterFileTypeDownloadPDF="//button[@data-tip='Download PDF']"
+
+EDQMMasterFileTypeDownloadExcel="//button[@data-tip='Download Excel']"
+
+EDQMMasterFileTypeName="//input[@id='smahname'][@name='smahname']"
+
+EDQMMasterFileDescription="*[@name='smasterfiletypedesc'][@id='smasterfiletypedesc']"
+
+EDQMMasterFileTypeAddSubmit="//button[text()='Save']"
+
+EDQMMasterFileTypeAddCancel="//button[text()='Cancel']"
+
+EDQMMasterFileTypeEditSubmit="//button[text()='Save']"
+
+EDQMMasterFileTypeEditCancel="//button[text()='Cancel']"
+
+module="Master"
+
+subModule="EDQM Master"
+
+screen="EDQM Master File Type"
